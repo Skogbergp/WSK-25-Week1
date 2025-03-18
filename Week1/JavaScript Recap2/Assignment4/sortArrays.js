a3c6d02bed39fe9a4a4b3716477a1a2bfea4d586
Patrik Skogberg <skogbergp@gmail.com>
2025-03-18 10:57:44 +0200

    Refactor array sorting functions in Assignment 4 and Assignment 5 for improved readability and added descending order option

--- Week1/JavaScript Recap2/Assignment4/sortArrays.js
+++ Week1/JavaScript Recap2/Assignment4/sortArrays.js
@@ -1,9 +1,11 @@
 var numbers = [1, 131, 231, 42, 53, 1, 54, 553, 3, 2];
 
 console.log('unsorted: ' + numbers);
-// Sort the array in ascending order
-numbers.sort(function (a, b) {
-  return a - b;
-});
 
-console.log('sorted: ' + numbers);
+function sortArray(arr) {
+  return arr.sort(function (a, b) {
+    return a - b;
+  });
+}
+
+console.log('sorted: ' + sortArray(numbers));
--- Week1/JavaScript Recap2/Assignment5/sortArrays.js
+++ Week1/JavaScript Recap2/Assignment5/sortArrays.js
@@ -1,9 +1,17 @@
 var numbers = [1, 131, 231, 42, 53, 1, 54, 553, 3, 2];
 
 console.log('unsorted: ' + numbers);
-// Sort the array in ascending order
-numbers.sort(function (a, b) {
-  return a - b;
-});
 
-console.log('sorted: ' + numbers);
+function sortArray(arr, order) {
+  if (order === 'desc') {
+    return arr.sort(function (a, b) {
+      return b - a;
+    });
+  }
+  return arr.sort(function (a, b) {
+    return a - b;
+  });
+}
+
+console.log('sorted desc: ' + sortArray(numbers, 'desc'));
+console.log('sorted asc: ' + sortArray(numbers, 'asc'));
