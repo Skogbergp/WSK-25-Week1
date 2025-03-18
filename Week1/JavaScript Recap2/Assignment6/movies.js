dedddca1f72821d586d1a666f3b5010b48b48e38
Patrik Skogberg <skogbergp@gmail.com>
2025-03-18 11:21:13 +0200

    Refactor movie input handling and update sorting logic to display highest rated movie

--- Week1/JavaScript Recap2/Assignment6/movies.js
+++ Week1/JavaScript Recap2/Assignment6/movies.js
@@ -3,18 +3,18 @@ var movies = [];
 var input = prompt('Enter the number of movies you want to add: ');
 
 for (var i = 0; i < input; i++) {
-  var name = prompt('Enter the movie name: ');
+  var movieName = prompt('Enter the movie name: ');
   var rating = parseFloat(prompt('Enter the movie rating: '));
 
   var movie = {
-    name: name,
+    name: movieName,
     rating: rating,
   };
   movies.push(movie);
 }
 
 movies.sort(function (a, b) {
-  return a.rating - b.rating;
+  return b.rating - a.rating;
 });
 
 const sortedMovies = document.createElement('p');
@@ -23,4 +23,12 @@ sortedMovies.innerHTML = JSON.stringify(movies);
 
 console.log(movies);
 
-console.log('highest rated movie: ' + movies[movies.length - 1].name);
+console.log('highest rated movie: ' + movies[0].name);
+
+const highestRatedMovie = document.createElement('p');
+document.body.appendChild(highestRatedMovie);
+highestRatedMovie.innerHTML =
+  'highest rated movie: ' +
+  movies[0].name +
+  ' with rating: ' +
+  movies[0].rating;
